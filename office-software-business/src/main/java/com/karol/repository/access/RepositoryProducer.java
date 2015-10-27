f7abb44f8b2e5b34f67379899a7a96fad85afa0d
Karol Stańczyk <karol.stanczyk@comarch.com>
2015-10-27 13:09:11 +0100

    Added log4j.

--- office-software-business/src/main/java/com/karol/repository/access/RepositoryProducer.java
+++ office-software-business/src/main/java/com/karol/repository/access/RepositoryProducer.java
@@ -5,13 +5,17 @@ import com.karol.repository.ContractorRepository;
 import com.karol.repository.utils.DatabaseException;
 import net.sf.cglib.proxy.Enhancer;
 import net.sf.cglib.proxy.InvocationHandler;
+import org.apache.log4j.Logger;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
 import java.lang.reflect.Executable;
+import java.lang.reflect.Method;
 
 @Singleton
 public class RepositoryProducer {
+
+    private static final Logger log = Logger.getLogger(RepositoryProducer.class);
 
     @Inject
     private EntityManager entityManager;
@@ -34,9 +38,11 @@ public class RepositoryProducer {
                 (InvocationHandler) (proxy, method, args) -> {
                     if (hasAnnotation(method, Transactional.class)) {
                         try {
+                            log.debug("Transaction for method" + getMethodName(method) + "start.");
                             entityManager.getTransaction().begin();
                             Object result = method.invoke(object, args);
                             entityManager.getTransaction().commit();
+                            log.debug("Transaction for method" + getMethodName(method) + "commit.");
                             return result;
                         } catch (Exception e) {
                             entityManager.getTransaction().rollback();
@@ -46,6 +52,10 @@ public class RepositoryProducer {
                         return method.invoke(object, args);
                     }
                 });
+    }
+
+    private String getMethodName(Method method) {
+        return " '" + method.getDeclaringClass().getSimpleName() + "." + method.getName() + "' ";
     }
 
     private boolean hasAnnotation(Executable executable, Class c) {
